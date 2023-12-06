194b7aeca732a3280f5f10ede910cb0633254c62
Sch3lp <sch3lp@gmail.com>
2023-12-06 21:59:49 +0100

    Day 3 part 2 example input works, but actual doesn't

--- kotlin/src/test/kotlin/be/swsb/aoc2023/day3/Solve.kt
+++ kotlin/src/test/kotlin/be/swsb/aoc2023/day3/Solve.kt
@@ -3,6 +3,7 @@ package be.swsb.aoc2023.day3
 import be.swsb.aoc2023.Point
 import be.swsb.aoc2023.readFile
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -18,30 +19,37 @@ class Solve {
     }
 
     @Test
-    fun `parse to schematic`() {
+    fun `solve part 2`() {
+        solve2(readFile(exampleInput)) shouldBe 467835
+        solve2(readFile(actualInput)) shouldBeGreaterThan 49062287
+    }
+
+    @Test
+    fun `schematic's allNumbers`() {
         val schematic: Schematic = readFile(exampleInput).parse()
 
-        schematic.allNumbers().map { it.first } shouldContainExactlyInAnyOrder listOf(
-            467,
-            35,
-            633,
-            617,
-            592,
-            755,
-            664,
-            598,
-            114,
-            58
+        schematic.allNumbers()
+            .map { it.first } shouldContainExactlyInAnyOrder listOf(467, 35, 633, 617, 592, 755, 664, 598, 114, 58)
+    }
+
+    @Test
+    fun `schematic's allNumbers adjacent to a symbol`() {
+        val schematic: Schematic = readFile(exampleInput).parse()
+        schematic.allNumbersAdjacentToSymbol()
+            .map { it.first } shouldContainExactlyInAnyOrder listOf(467, 35, 633, 617, 592, 755, 664, 598)
+    }
+
+    @Test
+    fun `schematic's gears`() {
+        val schematic: Schematic = readFile(exampleInput).parse()
+        val allGears = schematic.allGears()
+        allGears shouldContainExactlyInAnyOrder listOf(
+            Point(3, 1) to setOf(467,35),
+            Point(5, 8) to setOf(755,598),
         )
-        schematic.allNumbersAdjacentToSymbol().map { it.first } shouldContainExactlyInAnyOrder listOf(
-            467,
-            35,
-            633,
-            617,
-            592,
-            755,
-            664,
-            598
+        allGears.map { it.ratio } shouldContainExactlyInAnyOrder listOf(
+            16345,
+            451490,
         )
     }
 }
@@ -53,16 +61,31 @@ private fun String.parse() =
         }
     }.toMap())
 
+typealias Gear = Pair<Point, Set<Int>>
+val Gear.ratio get() = this.second.reduce(Int::times)
 
 class Schematic(private val points: Map<Point, Char>) {
 
-    fun allNumbersAdjacentToSymbol(): List<Pair<Int, List<Point>>> =
+    fun allGears(): List<Gear> {
+        val numbersNextToAsterisks = allNumbersAdjacentToSymbol('*').toMap()
+        val allAsterisks = points.filterValues { it == '*' }.keys
+        return allAsterisks.map { asterisk ->
+            asterisk to numbersNextToAsterisks.filterValues { points -> points.any { point -> point in asterisk.neighbours } }.keys
+        }.filter { (_,numbers) -> numbers.size >= 2 }
+    }
+
+    fun allNumbersAdjacentToSymbol(symbol: Char? = null): List<Pair<Int, List<Point>>> =
         allNumbers().filter { (_, points) ->
-            points.any { point -> isAdjacentToASymbol(point) }
+            points.any { point -> point.isAdjacentToASymbol(symbol) }
         }
 
-    private fun isAdjacentToASymbol(point: Point) =
-        point.neighbours.any { neighbour -> neighbour in this.points.filterValues { !it.isDigit() && it != '.' }.keys }
+    private fun Point.isAdjacentToASymbol(symbol: Char? = null) =
+        neighbours.any { neighbour ->
+            neighbour in this@Schematic.points.filterValues {
+                if (symbol == null) !it.isDigit() && it != '.'
+                else it == symbol
+            }.keys
+        }
 
     fun allNumbers(): List<Pair<Int, List<Point>>> {
         val maxX = points.maxOf { (k, _) -> k.x }
@@ -90,4 +113,5 @@ class Schematic(private val points: Map<Point, Char>) {
     }
 }
 
-fun solve(string: String) = string.parse().allNumbersAdjacentToSymbol().sumOf { (n,_) -> n }
+fun solve(string: String) = string.parse().allNumbersAdjacentToSymbol().sumOf { (n, _) -> n }
+fun solve2(string: String) = string.parse().allGears().sumOf { it.ratio }
