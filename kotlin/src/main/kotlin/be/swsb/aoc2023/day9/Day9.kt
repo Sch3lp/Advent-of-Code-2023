31ceb6aa1c80d78661b630042ef67b8385e583be
Sch3lp <sch3lp@gmail.com>
2023-12-09 22:24:17 +0100

    Refactor

--- kotlin/src/main/kotlin/be/swsb/aoc2023/day9/Day9.kt
+++ kotlin/src/main/kotlin/be/swsb/aoc2023/day9/Day9.kt
@@ -3,11 +3,7 @@ package be.swsb.aoc2023.day9
 fun generateNextSequenceFrom(sequence: List<Long>): List<Long> =
     sequence.zipWithNext().fold(listOf()) { acc, (m1, m2) -> acc + (m2 - m1) }
 
-fun predict(sequence: List<Long>, backwards : Boolean = false): Long {
-    return predictForwards(sequence, backwards)
-}
-
-private fun predictForwards(sequence: List<Long>, backwards: Boolean = false): Long {
+fun predict(sequence: List<Long>, backwards: Boolean = false): Long {
     var curSequence = if (backwards) sequence.reversed() else sequence
     val lastMeasurements = mutableListOf(curSequence.last())
     while (!curSequence.all { it == 0L }) {
